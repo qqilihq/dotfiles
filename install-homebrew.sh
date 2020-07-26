f12ca1b4cbd3180c592281aaba59d084ff72d345
Philipp Katz <philipp@philippkatz.de>
2020-07-26 15:29:52 +0200

    brew: additional casks

--- install-homebrew.sh
+++ install-homebrew.sh
@@ -3,4 +3,19 @@
 brew install cliclick
 brew install titlecase
 
+brew cask install 1password
+brew cask install docker
+brew cask install easyfind
+brew cask install firefox
+brew cask install fork
+brew cask install google-chrome
+brew cask install grandperspective
+brew cask install keka
+brew cask install mactracker
+brew cask install nextcloud
+brew cask install paw
 brew cask install phoenix
+brew cask install robo-3t
+brew cask install sublime-text
+brew cask install transmit
+brew cask install visual-studio-code
