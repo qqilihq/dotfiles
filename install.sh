a5295f900f833262daf244a3a99f71af57d4f7fb
Philipp Katz <philipp@philippkatz.de>
2023-09-28 10:02:33 +0200

    Fix filename

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ symlink_if_not_exists "${PWD}/.bash_profile" "${HOME}/.bash_profile"
 symlink_if_not_exists "${PWD}/.phoenix.js" "${HOME}/.phoenix.js"
 symlink_if_not_exists "${PWD}/.inputrc" "${HOME}/.inputrc"
 symlink_if_not_exists "${PWD}/.zshrc" "${HOME}/.zshrc"
-symlink_if_not_exists "${PWD}/.toolchains.xml" "${HOME}/.m2/toolchains.xml"
+symlink_if_not_exists "${PWD}/toolchains.xml" "${HOME}/.m2/toolchains.xml"
 
 # put ssh config in iCloud Drive -> `./sync/.ssh_config`
 mkdir -p "${HOME}/.ssh"
