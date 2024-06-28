0eac898cb9a0f44836f71f787a12afb8925001e4
Philipp Katz <philipp@philippkatz.de>
2024-06-28 11:58:27 +0200

    oasdiff

--- install-software.sh
+++ install-software.sh
@@ -65,3 +65,7 @@ mas install 1437501942 # RSS Button for Safari
 mas install 409201541 # Pages
 mas install 409203825 # Numbers
 mas install 409183694 # Keynote
+
+# oasdiff
+brew tap tufin/homebrew-tufin
+brew install oasdiff
